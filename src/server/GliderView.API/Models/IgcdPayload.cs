d61bb8cb2a4d61eac9862fd364d2dcade305ee5d
Ryan Tate <ryantate314@gmail.com>
2022-11-18 19:00:49 -0500

    FIx Landing Type... Again

--- src/server/GliderView.API/Models/IgcdPayload.cs
+++ src/server/GliderView.API/Models/IgcdPayload.cs
@@ -13,7 +13,7 @@
         public const string TYPE_TESTHOOK = "testhook";
         public const string TYPE_LANDING = "landing";
         public const string TYPE_TAKEOFF = "takeoff";
-        public const string TYPE_UNDEFINEDTAKEOFF = "udlanding";
-        public const string TYPE_UNDEFINEDLANDING = "udtakeoff";
+        public const string TYPE_UNDEFINEDTAKEOFF = "udtakeoff";
+        public const string TYPE_UNDEFINEDLANDING = "udlanding";
     }
 }
