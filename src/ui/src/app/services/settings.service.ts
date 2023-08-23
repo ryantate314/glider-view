428842b60d1e6c6400141e769fa54514922375f4
Ryan Tate <ryantate314@gmail.com>
2023-08-23 14:55:03 -0400

    Refactor Pricing Toggle to Observable and Extend Expiration

--- src/ui/src/app/services/settings.service.ts
+++ src/ui/src/app/services/settings.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
 import { DisplayMode } from '../models/display-mode';
 import { CookieService } from 'ngx-cookie';
+import * as dayjs from 'dayjs';
+import { AuthService } from './auth.service';
+import { Observable, Subject, combineLatest, distinctUntilChanged, map, of, shareReplay, startWith, switchMap, tap, withLatestFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
 
+  private _updateShouldShowPricing$ = new Subject<void>();
+  public shouldShowPricing$: Observable<boolean>;
+
   constructor(
-    private readonly cookies: CookieService
-  ) { }
+    private readonly cookies: CookieService,
+    private readonly auth: AuthService
+  ) {
+    this.shouldShowPricing$ = combineLatest([
+      this.auth.user$,
+      this._updateShouldShowPricing$.pipe(
+        startWith(null)
+      )
+    ]).pipe(
+      map(([user, _]) => {
+        if (user === null)
+          return false;
+        else
+          return this.cookies.get(this.generateCookieName(user.userId, "showPricing")) === "true"
+      }),
+      distinctUntilChanged(),
+      shareReplay(1)
+    );
+  }
 
   private getItem(key: string): string | null {
     return sessionStorage.getItem(key);
@@ -37,14 +60,32 @@ export class SettingsService {
     return value ?? DisplayMode.Table;
   }
 
-  public get showPricing(): boolean | null {
-    const value = this.cookies.get('showPricing');
-    return value === undefined
-      ? null
-      : value === "true";
+  private generateCookieName(userId: string, name: string): string {
+    return `${userId}-${name}`;
   }
 
-  public set showPricing(value: boolean | null) {
-    this.cookies.put('showPricing', (!!value) ? "true" : "false");
+  public setShouldShowPricing(value: boolean | null): Observable<void> {
+    const result = of(true).pipe(
+      withLatestFrom(this.auth.user$.pipe(
+        startWith(null)
+      )),
+      tap(([_, user]) => {
+        if (user !== null) {
+          this.cookies.put(
+            this.generateCookieName(user.userId, "showPricing"),
+            (!!value) ? "true" : "false",
+            {
+              expires: dayjs().add(5, 'year').toDate()
+            }
+          );
+          this._updateShouldShowPricing$.next();
+        }
+        else
+          console.log("Cannot set preferences. User not logged in.");
+      }),
+      switchMap(_ => of())
+    );
+    result.subscribe();
+    return result;
   }
 }
